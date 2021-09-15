b487978d878f72f990106a8e83e46da07e9091e0
alifr <frank.ali@nih.gov>
2021-09-15 14:44:10 -0400

    adding advancedRoller component

---
+++ index.js
@@ -0,0 +1,2 @@
+export { default as DisplayResults } from './src/displayResults'
+export { default as AdvancedRoller } from './src/advancedRoller'
---
+++ src/advancedRoller/advancedRoller.js
@@ -0,0 +1,67 @@
+import './advancedRoller.css'
+import DiceParser from '@3d-dice/fdp'
+
+const noop = () => {}
+
+class AdvancedRoller{
+	constructor(options){
+		this.target = options.target ? document.querySelector(options.target) : document.body
+		this.elem = document.createRange().createContextualFragment(`
+			<div class="adv-roller">
+				<form class="adv-roller--form">
+					<input class="adv-roller--notation" placeholder="2d20" autocomplete="off" />
+					<input class="adv-roller--clear" type="reset" value="Clear" />
+				</form>
+			</div>
+		`)
+		this.form = this.elem.querySelector('.adv-roller--form')
+
+		// create Notation Parser
+		this.DRP = new DiceParser()
+
+		// callback events
+		this.onSubmit = options.onSubmit || noop
+		this.onClear = options.onClear || noop
+		this.onReroll = options.onReroll || noop
+		this.onResults = options.onResults || noop
+		this.init()
+	}
+
+	init(){
+		this.form.addEventListener("submit",this.submitForm.bind(this))
+		this.form.addEventListener("reset",this.clear.bind(this))
+
+		this.target.prepend(this.elem)
+	}
+
+	submitForm(e) {
+		e.preventDefault();
+		this.clear()
+		this.onSubmit(this.DRP.parseNotation(this.form.firstElementChild.value))
+	}
+	clear(){
+		this.DRP.clear()
+		if(this.onClear){
+			this.onClear()
+		}
+	}
+	handleResults(results){
+		const rerolls = this.DRP.handleRerolls(results)
+		if(rerolls.length) {
+			this.onReroll(rerolls)
+			return rerolls
+		}
+
+		const finalResults = this.DRP.parsedNotation ? this.DRP.parseFinalResults(results) : results
+
+		// dispatch an event with the results object for other UI elements to listen for
+		const event = new CustomEvent('resultsAvailable', {detail: finalResults})
+		document.dispatchEvent(event)
+
+		this.onResults(finalResults)
+		
+		return finalResults
+	}
+}
+
+export default AdvancedRoller
