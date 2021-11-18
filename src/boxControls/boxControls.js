d09f8ce8ea3ac442f7810927b8b6527ffab6ff51
alifr <frank.ali@nih.gov>
2021-11-18 09:06:00 -0500

    add themes list config param

--- src/boxControls/boxControls.js
+++ src/boxControls/boxControls.js
@@ -2,9 +2,12 @@ import * as dat from 'dat.gui'
 
 const noop = () => {}
 
+//TODO: handle placement of controls in dice-box dom element - createElement wrapper and appendChild using constructor options
 class BoxControls {
 	constructor(options){
-		this.gui = new dat.GUI()
+		this.gui = new dat.GUI({
+			autoPlace: true
+		})
 		this.config = {
 			enableShadows: true,
 			delay: 10,
@@ -17,9 +20,13 @@ class BoxControls {
 			startingHeight: 15,
 			settleTimeout: 5000,
 			spinForce: 6,
-			throwForce: 2,
+			throwForce: 4,
 			zoomLevel: 3,
 			theme: '#0974E6',
+			themes: [
+				'purpleRock',
+				'diceOfRolling'
+			]
 		}
 		// callback
 		this.onUpdate = options?.onUpdate || noop
@@ -43,10 +50,12 @@ class BoxControls {
 		const f2 = this.gui.addFolder('Rendering')
 		f2.add(this.config,'delay',10,500,10).onChange(this.handleUpdate.bind(this))
 		f2.add(this.config,'zoomLevel',0,7,1).onChange(this.handleUpdate.bind(this))
-		f2.add(this.config,'theme',['purpleRock']).onChange(this.handleUpdate.bind(this))
+		f2.add(this.config,'theme',this.config.themes).onChange(this.handleUpdate.bind(this))
 		f2.addColor(this.config, 'theme').onChange(this.handleUpdate.bind(this))
 		f2.add(this.config,'enableShadows').onChange(this.handleUpdate.bind(this))
 		f2.open()
+
+		this.gui.close()
 	}
 	handleUpdate(e){
 		this.onUpdate(this.config)
