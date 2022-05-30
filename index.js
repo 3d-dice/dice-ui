6397aab3deacebc1f6457487c5fcdae35223237a
alifr <frank.ali@nih.gov>
2022-05-30 09:10:33 -0400

    move index file inside src folder

--- index.js
+++
@@ -1,4 +0,0 @@
-export { default as DisplayResults } from './src/displayResults'
-export { default as AdvancedRoller } from './src/advancedRoller'
-export { default as BoxControls } from './src/boxControls'
-export { default as DicePicker } from './src/dicePicker'
---
+++ src/index.js
@@ -0,0 +1,4 @@
+export { default as DisplayResults } from './displayResults'
+export { default as AdvancedRoller } from './advancedRoller'
+export { default as BoxControls } from './boxControls'
+export { default as DicePicker } from './dicePicker'
